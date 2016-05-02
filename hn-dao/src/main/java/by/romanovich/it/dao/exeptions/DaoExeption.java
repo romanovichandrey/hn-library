e9374242388ca65308e6c2399f048083f483967b
romanovichandrey <romanovichandrey1989@gmail.com>
2016-05-02 14:58:11 +0300

    Create class DaoExeption

--- hn-dao/src/main/java/by/romanovich/it/dao/exeptions/DaoExeption.java
+++ hn-dao/src/main/java/by/romanovich/it/dao/exeptions/DaoExeption.java
@@ -1,5 +1,10 @@
 package by.romanovich.it.dao.exeptions;
 
+/**
+ * Create custom exception on dao layer.
+ * @author Romanovich Andrey
+ * @version 1.0
+ */
 public class DaoExeption extends Exception {
 
     private Exception exception;
