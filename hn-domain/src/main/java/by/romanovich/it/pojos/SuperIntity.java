e1cf4ef1f0e0bec172f07b7fe3044155d93424fd
romanovichandrey <romanovichandrey1989@gmail.com>
2016-05-02 14:02:58 +0300

    Create class SuperEntity

---
+++ hn-domain/src/main/java/by/romanovich/it/pojos/SuperIntity.java
@@ -0,0 +1,56 @@
+package by.romanovich.it.pojos;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * This is superclass for pojos
+ * @see java.io.Serializable
+ * @author Romanovich Andrey
+ * @version 1.0
+ */
+@MappedSuperclass
+@SequenceGenerator(name = "PK")
+public class SuperIntity implements Serializable {
+
+    private static final long serialVersionUID = 4L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "PK")
+    private long id;
+
+    public SuperIntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SuperIntity)) return false;
+
+        SuperIntity that = (SuperIntity) o;
+
+        if (id != that.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "SuperIntity{" +
+                "id=" + id +
+                '}';
+    }
+}
