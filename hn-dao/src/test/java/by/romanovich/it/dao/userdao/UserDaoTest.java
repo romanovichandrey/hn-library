f25f3c2260f85bc31348d1b2681c9489af52f5f7
romanovichandrey <romanovichandrey1989@gmail.com>
2016-05-04 20:40:26 +0300

    Create class UserDaoTest

---
+++ hn-dao/src/test/java/by/romanovich/it/dao/userdao/UserDaoTest.java
@@ -0,0 +1,26 @@
+package by.romanovich.it.dao.userdao;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+}
