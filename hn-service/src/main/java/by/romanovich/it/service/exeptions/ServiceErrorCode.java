371909317fc3446a75b146c3548076e200e5361b
romanovichandrey <romanovichandrey1989@gmail.com>
2016-05-12 20:37:18 +0300

    add error for AutorService, BookService, CategoryService

--- hn-service/src/main/java/by/romanovich/it/service/exeptions/ServiceErrorCode.java
+++ hn-service/src/main/java/by/romanovich/it/service/exeptions/ServiceErrorCode.java
@@ -11,7 +11,22 @@ public enum  ServiceErrorCode {
     HN_SERV_002("Cannot delete user"),
     HN_SERV_003("Cannot update user"),
     HN_SERV_004("Cannot get user by login and password"),
-    HN_SERV_005("Cannot get all users");
+    HN_SERV_005("Cannot get all users"),
+    HN_SERV_006("Cannot update book"),
+    HN_SERV_007("Cannot get book by id"),
+    HN_SERV_008("Cannot get all books"),
+    HN_SERV_009("Cannot delete book"),
+    HN_SERV_010("Cannot add book"),
+    HN_SERV_011("Cannot update category"),
+    HN_SERV_012("Cannot get category by id"),
+    HN_SERV_013("Cannot get all categories"),
+    HN_SERV_014("Cannot delete category"),
+    HN_SERV_015("Cannot add category"),
+    HN_SERV_016("Cannot update autor"),
+    HN_SERV_017("Cannot get autor by id"),
+    HN_SERV_018("Cannot get all autors"),
+    HN_SERV_019("Cannot delete autor"),
+    HN_SERV_020("Cannot add autor");
 
 
     private final String value;
