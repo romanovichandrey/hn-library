5e464116dcbb6b2746d271f8d705f570618df588
romanovichandrey <romanovichandrey1989@gmail.com>
2016-05-19 14:52:34 +0300

    End

--- hn-web/src/main/java/by/romanovich/it/controller/LogOutController.java
+++ hn-web/src/main/java/by/romanovich/it/controller/LogOutController.java
@@ -16,10 +16,10 @@ import java.io.IOException;
 public class LogOutController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-
     public LogOutController() {
         super();
     }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession httpSession = request.getSession();
